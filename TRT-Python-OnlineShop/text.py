3911cb32e8478470aebb077432f567b61857b413
Michael Allen <mr.michael.g.allen@gmail.com>
2017-02-27 15:18:13 -0800

    Update text.py

--- TRT-Python-OnlineShop/text.py
+++ TRT-Python-OnlineShop/text.py
@@ -7,5 +7,16 @@ Line Six Added
 Line Seven Added
 Line Eight Added
 
+# Michael 01
+# Michael 02
+# Michael 03
+# Michael 04
+# Michael 05
+# Michael 06
+# Michael 07
+# Michael 08
+# Michael 09
+# Michael 10
+
 
 
