2a25d3a59a73523eb387e9fcbc6aaa0018621d45
Michael Allen <mr.michael.g.allen@gmail.com>
2017-02-27 13:12:57 -0800

    Create Online-Shop.py

---
+++ TRT-Python-OnlineShop/Online-Shop.py
@@ -0,0 +1,9 @@
+# Online Shop - Changes price based on hour and days
+# Flash sales website
+# Python 2.7
+# Version 0.1
+# Author: Tech Group (2/24/17) Annie Bowman, Matthew Perrow, Temple Naylor, Shabab Ali, and Michael Allen
+
+import time
+import datatime
+
