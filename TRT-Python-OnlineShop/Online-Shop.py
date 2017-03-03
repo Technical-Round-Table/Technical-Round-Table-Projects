d9301d401c53115867fa6aade6645a8a972c7ce7
mrmichaelgallen <mr.michael.g.allen@gmail.com>
2017-03-03 11:30:52 -0800

    March 3 - Contributions to Online-Shop
    Shabab Ali, Annie Bowman, Garrett Guevara, Mat Perrow, Temple Naylor and
    Michael Allen

--- TRT-Python-OnlineShop/Online-Shop.py
+++ TRT-Python-OnlineShop/Online-Shop.py
@@ -1,35 +1,58 @@
-# Online Shop - Changes price based on hour and days
-# Flash sales website
-# Python 3.6
-# Version 0.1
-# Author: Tech Group (2/24/17) Annie Bowman, Matthew Perrow, Temple Naylor, Shabab Ali, and Michael Allen
-
 import time
-import datatime
+import datetime
 
 #This is the first comment
 
-today = datetime.datetime.dayofweek()
+# today = datetime.datetime.dayofweek()
 
 # Raw Input "Welcome Message to Cheap Tech Supplies"
 
 name = input("Please enter your name: ")
 
-welcome = print("Welcome {} to Cheap Tech Supplies!".format(name))
+welcome = print("Hello {}! Welcome to Cheap Tech Supplies".format(name))
+
+departmentChoice = int(input("1. Hardware\
+                   \n2. Software\
+                   \n3. Accessories\
+                   \n4. Warranties\
+                   \n\nChoose a department: \n"))
+
+### Print List of each department with if, elif and else statements
+### Define Departments
+
+hardwareList = {'phone': 299.00,'monitor': 150.00,}
+softwareList = {'Windows 10': 100.00,'Microsoft Office': 200.00, }
+accessoriesList = {'Keyboard': 29.00,'HDMI Gold-Plated': 49.00, }
+warrantiesList = {'1-year': 29.00,'2-year': 50.00, '2-year': 50.00,}
+
+def getDepartment(departmentChoice):
+    if departmentChoice == 1:
+        print(hardwareList)
+        hardwareSelected = input("Welcome to Hardware, please choose an item")
+    elif departmentChoice == 2:
+        print(softwareList)
+        softwareSelected = input("Welcome to Software, please choose an item")
+    elif departmentChoice == 3:
+        print(accessoriesList)
+        softwareSelected = input("Welcome to Accessories, please choose an item")
+    elif departmentChoice == 4:
+        print(warrantiesList)
+        softwareSelected = input("Welcome to Warranties, please choose an item")
+    else:
+        print("Not a valid selection, please choose a number")
+
+getDepartment(departmentChoice)
 
 
-online_shop = {'phone': 299.00,'monitor': 150.00,'Keyboard': 29.00,'HDMI': 49.00, }
 
-cart = []
 
-cart.append()
+# online_shop = {}
+
+# cart = []
+
+# cart.append()
 
 # Rawinput
 
 
 #Find the price today
-
-
-
-
-
