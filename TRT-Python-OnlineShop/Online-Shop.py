e81051aae5fa9e75f50e063089715e84bcded8d1
gcguevara <garrettcguevara@gmail.com>
2017-03-03 12:06:45 -0800

    Refactor in regards to pep 8, convert function into while loop with entry sanitation and option to quit.

--- TRT-Python-OnlineShop/Online-Shop.py
+++ TRT-Python-OnlineShop/Online-Shop.py
@@ -1,47 +1,53 @@
 import time
 import datetime
 
-#This is the first comment
 
-# today = datetime.datetime.dayofweek()
+# Print List of each department with if, elif and else statements
+# Define Departments
 
-# Raw Input "Welcome Message to Cheap Tech Supplies"
+# global variables that contain dictionaries
+hardware_list = {'phone': 299.00,'monitor': 150.00,}
+software_list = {'Windows 10': 100.00,'Microsoft Office': 200.00, }
+accessories_list = {'Keyboard': 29.00,'HDMI Gold-Plated': 49.00, }
+warranties_list = {'1-year': 29.00,'2-year': 50.00, '2-year': 50.00,}
 
-name = input("Please enter your name: ")
+def get_department():
+    # welcome message
+    name = input("Please enter your name: ")
+    print("Hello {}! Welcome to Cheap Tech Supplies".format(name))
 
-welcome = print("Hello {}! Welcome to Cheap Tech Supplies".format(name))
+    department_choice = None
+    while department_choice != 5:
+        print("Please enter 1, 2, 3, or 4 to pick a department or enter 5 to quit.\n")
+        try:
+            department_choice = int(input("1. Hardware\
+                                   \n2. Software\
+                                   \n3. Accessories\
+                                   \n4. Warranties\
+                                   \n\nChoose a department: \n"))
 
-departmentChoice = int(input("1. Hardware\
-                   \n2. Software\
-                   \n3. Accessories\
-                   \n4. Warranties\
-                   \n\nChoose a department: \n"))
+            if department_choice == 1:
+                print(hardware_list)
+                # hardwareSelected = input("Welcome to Hardware, please choose an item")
+            elif department_choice == 2:
+                print(software_list)
+                # softwareSelected = input("Welcome to Software, please choose an item")
+            elif department_choice == 3:
+                print(accessories_list)
+                # softwareSelected = input("Welcome to Accessories, please choose an item")
+            elif department_choice == 4:
+                print(warranties_list)
+                # softwareSelected = input("Welcome to Warranties, please choose an item")
+            elif department_choice == 5:
+                break
 
-### Print List of each department with if, elif and else statements
-### Define Departments
+        except (ValueError):
+            print("That is not a valid entry, please try again.\n")
+            continue
 
-hardwareList = {'phone': 299.00,'monitor': 150.00,}
-softwareList = {'Windows 10': 100.00,'Microsoft Office': 200.00, }
-accessoriesList = {'Keyboard': 29.00,'HDMI Gold-Plated': 49.00, }
-warrantiesList = {'1-year': 29.00,'2-year': 50.00, '2-year': 50.00,}
 
-def getDepartment(departmentChoice):
-    if departmentChoice == 1:
-        print(hardwareList)
-        hardwareSelected = input("Welcome to Hardware, please choose an item")
-    elif departmentChoice == 2:
-        print(softwareList)
-        softwareSelected = input("Welcome to Software, please choose an item")
-    elif departmentChoice == 3:
-        print(accessoriesList)
-        softwareSelected = input("Welcome to Accessories, please choose an item")
-    elif departmentChoice == 4:
-        print(warrantiesList)
-        softwareSelected = input("Welcome to Warranties, please choose an item")
-    else:
-        print("Not a valid selection, please choose a number")
 
-getDepartment(departmentChoice)
+get_department()
 
 
 
